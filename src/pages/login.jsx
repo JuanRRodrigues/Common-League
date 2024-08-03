78c070456181551f2e94d87ab20a9e86df81902e
Juan R. Rodrigues <juancontato27@gmail.com>
2024-08-03 14:43:46 -0300

    Login and Register Interface V2

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,17 +3,58 @@ import EstilosGlobais from "../componentes/GlobaStyle";
 import Login from "../componentes/Login";
 import * as Components from '../componentes/Login/Components'
 import React from 'react';
+import http from "../http";
+import { Navigate, useNavigate } from "react-router-dom";
+import { Formik } from "formik";
+
+
+const Backgroundgradient = styled.div`
+background: linear-gradient(174.61deg, #141d26 4.16%, #1a2633 48%, #151515 96.76%);
+width: 100%;
+min-height: 100vh;
+`
 
 const App = () => {
+  const navigate = useNavigate();
   const [signIn, toggle] = React.useState(true);
   return (
+    <Backgroundgradient>
+    <EstilosGlobais />
+    <Formik 
+    
+      initialValues={{
+        login:'',
+        password:'',
+      }}
+      onSubmit={(values, actions) => {
+        console.log(values);
+        http.post('auth/login'. values, {})
+          .then(response => {
+            sessionStorage.removeItem('token');
+            sessionStorage.setItem('token', response.data.token);
+            localStorage.setItem('login', values.login)
+            console.log(response.data);
+            actions.resetForm();
+            navigate('/');
+          })
+            .catch(error => {
+              console.error('Error', error)
+            })
+      }}
+    >
     <Components.Container>
       <Components.SignUpContainer signinIn={signIn}>
         <Components.Form>
             <Components.Title>Create Account</Components.Title>
-            <Components.Input type="text" placeholder="Name" />
-            <Components.Input type="text" placeholder="Email" />
-            <Components.Input type="text" placeholder="Password" />
+            <Components.Input 
+            type="text" 
+            placeholder="Name" />
+            <Components.Input 
+            type="text" 
+            placeholder="Email" />
+            <Components.Input 
+            type="text" 
+            placeholder="Password" />
             <Components.Button>Sign Up</Components.Button>
         </Components.Form>
       </Components.SignUpContainer>
@@ -21,8 +62,12 @@ const App = () => {
       <Components.SignInContainer signinIn={signIn}>
           <Components.Form>
           <Components.Title>Sign In</Components.Title>
-            <Components.Input type="text" placeholder="Name" />
-            <Components.Input type="password" placeholder="Password" />
+            <Components.Input 
+            type="text" 
+            placeholder="Name" />
+            <Components.Input 
+            type="password" 
+            placeholder="Password" />
             <Components.Anchor href="#">Forgot Your Password</Components.Anchor>
             <Components.Button>Sign In</Components.Button>
           </Components.Form>
@@ -51,7 +96,10 @@ const App = () => {
           </Components.Overlay>
       </Components.OverlayContainer>
     </Components.Container>
+    </Formik>
+  </Backgroundgradient>
   )
 }
+
 
 export default App;
