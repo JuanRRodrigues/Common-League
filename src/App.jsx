75f3115a2ab398419b700da058db589875b786d8
Juan R. Rodrigues <juancontato27@gmail.com>
2024-08-08 12:53:28 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Routes, Route, BrowserRouter} from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/newHome/home';
 import Login from './pages/login';
 import Riot from './pages/riot'
 import Cadastro from './pages/cadastro'
