0a30042e6c2edc5f4dde9c7a65502ab566ffac1e
Math9751 <matthewbelt.mc@gmail.com>
2024-09-19 14:12:54 -0300

    Styled components

--- src/componentes/TeamRegister/index.jsx
+++ src/componentes/TeamRegister/index.jsx
@@ -7,21 +7,6 @@ import TeamService from '../../service/teamService';
 import { toast } from 'react-toastify';
 import Grid from '@mui/material/Grid';
 
-const StyledForm = styled(FormikForm)`
-    max-width: 400px;
-    margin: auto;
-    padding: 20px;
-    border-radius: 8px;
-    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-    background-color: white;
-    color: black;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    min-height: 100vh;
-`;
-
 export default function TeamsForm({ handleClose, row, option = 'add' }) {
     const ServiceTeam = new TeamService();
 
